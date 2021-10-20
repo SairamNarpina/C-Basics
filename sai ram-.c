f5a0229d6a0e82543ed38c5b921069bbfe3898e4
SairamNarpina <92844054+SairamNarpina@users.noreply.github.com>
2021-10-20 12:27:42 +0530

    Add files via upload

--- sai ram-.c
+++ sai ram-.c
@@ -1,18 +1,9 @@
 #include<stdio.h>
 void main()
 {
-	// varible decleration
-	int age;
-	float cgpa;
-	char initial;
+	// varible decleration cum initialization
+	int number1=143,number2=123,number3=231;
 	
-	// initilaization
-	age=21;
-	cgpa=8.6;
-	initial='R';
-	
-	// output 
-	printf("age of student:%d\n",age);
-	printf("cgpa of student:%f\n",cgpa);
-	printf("%c",initial);
+	// output
+	printf("addition of %d , %d ,is subtracted %d is %d", number1,number2,number3, number1+number2-number3);
 }

badc737cbb9dc2684bd11771a16d9328da04dcb8
SairamNarpina <92844054+SairamNarpina@users.noreply.github.com>
2021-10-20 12:09:32 +0530

    Add files via upload

---
+++ sai ram-.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	// varible decleration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// initilaization
+	age=21;
+	cgpa=8.6;
+	initial='R';
+	
+	// output 
+	printf("age of student:%d\n",age);
+	printf("cgpa of student:%f\n",cgpa);
+	printf("%c",initial);
+}

17aec2e3cb248b84f08f347615cb35e6037f4835
SairamNarpina <92844054+SairamNarpina@users.noreply.github.com>
2021-11-09 11:44:41 +0530

    Add files via upload

---
+++ scanf  for several numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int s,a,i,r,A,m;
+	printf("Enter six number:");
+	scanf("%d%d%d%d%d%d",&s,&a,&i,&r,&A,&m);
+	printf("number 1:%d\n",s);
+	printf("number 2:%d\n",a);
+	printf("number 3:%d\n",i);
+	printf("number 4:%d\n",r);
+	printf("number 5:%d\n",A);
+	printf("number 6:%d",m);
+}
